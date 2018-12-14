d7bbbd3fab708793606de10537ad7d86d08e018f
evanwill <evanpeterw@gmail.com>
2018-12-14 13:49:25 -0800

    add theme

---
+++ _site/assets/js/lunr-store.js
@@ -0,0 +1,137 @@
+var store = [ 
+     
+    { 
+        "indexId": "test001",
+        "title": "Administration Building, University of Idaho, No. 30",
+        "creator": "Pacific Photo Co.",
+        "subjects": ["universities","buildings","campuses","picture postcards"],
+        "date": "1910",
+        "description": "Photographic postcard of the University of Idaho administration building in Moscow, Idaho.",
+        "place": "Moscow, Idaho"
+    }, 
+    { 
+        "indexId": "test002",
+        "title": "Noll Aduitorium [Bryan Hall]",
+        "creator": "Patton Post Card Co.",
+        "subjects": ["universities","buildings","clock towers (towers)","picture postcards"],
+        "date": "1915-11-03",
+        "description": "Photographic postcard of Bryan Hall on the Washington State University campus (mislabeled 'Noll Aduitorium'). Card is damaged and held together with tape.",
+        "place": "Pullman, Washington"
+    }, 
+    { 
+        "indexId": "test003",
+        "title": "University farms",
+        "creator": null,
+        "subjects": ["campuses","farms","horse-drawn vehicles"],
+        "date": "1895",
+        "description": "1895 photograph of University Farms. Moscow in background. [PG1_105-15]",
+        "place": "Moscow, Idaho"
+    }, 
+    { 
+        "indexId": "test004",
+        "title": "University farms, University of Idaho in distance",
+        "creator": null,
+        "subjects": ["campuses","farms","fields (land)"],
+        "date": "1900",
+        "description": "1900 photograph of University Farms. Spanish American War Memorial in background. [PG1_105-16]",
+        "place": "Moscow, Idaho"
+    }, 
+    { 
+        "indexId": "test005",
+        "title": "Administration Building, Washington State College",
+        "creator": "Johnson",
+        "subjects": ["universities","buildings","campuses","picture postcards"],
+        "date": "1905",
+        "description": "Photographic postcard of the administration building on the Washington State University campus in Pullman, Washington",
+        "place": "Pullman, Washington"
+    }, 
+    { 
+        "indexId": "test006",
+        "title": "Tacoma High School, Tacoma, Washington",
+        "creator": "Central News Company",
+        "subjects": ["high schools (buildings)","historic buildings","trees","horse-drawn vehicles","picture postcards","flagpoles","flags"],
+        "date": "1909-07-17",
+        "description": "Postcard is of a Tacoma, Washington High School, now named Stadium High School. Postmark 7/17/1909, Tacoma, WA. Destination Helena, MT.",
+        "place": "Tacoma, Washington"
+    }, 
+    { 
+        "indexId": "test007",
+        "title": "Pierce County Court House, Tacoma, Washington",
+        "creator": "Portland Post Card Co.",
+        "subjects": ["public buildings","county courthouses","trees","historic buildings","picture postcards"],
+        "date": "1911-02-23",
+        "description": "Postcard is of the old Pierce County Courthouse in Tacoma, Washington. The building was demolished in 1959. Postmark 2/23/1911, Tacoma, WA. Destination Los Angeles, CA.",
+        "place": "Tacoma, Washington"
+    }, 
+    { 
+        "indexId": "test008",
+        "title": "Spokane County Court House, Spokane, Washington",
+        "creator": "Spokane Post Card Co.",
+        "subjects": ["public buildings","county courthouses","trees","picture postcards"],
+        "date": "1912-09-08",
+        "description": "Postcard is of the Spokane County Courthouse in Spokane, Washington. Postmark 9/8/1912, Spokane, WA. Destination San Francisco, CA.",
+        "place": "Spokane, Washington"
+    }, 
+    { 
+        "indexId": "test009",
+        "title": "Whatcom County Court House, Bellingham, Washington",
+        "creator": "Sandison, Lowman & Hanford Co.",
+        "subjects": ["public buildings","county courthouses","trees","picture postcards"],
+        "date": "1920",
+        "description": "Postcard of the Whatcom County Courthouse in Bellingham, Washington.",
+        "place": "Bellingham, Washington"
+    }, 
+    { 
+        "indexId": "test010",
+        "title": "Hotel Seattle, Seattle, Washington",
+        "creator": null,
+        "subjects": ["hotels (public accommodations)","buildings","men","streetcars","photographic"],
+        "date": "1901",
+        "description": "Postcard is of the Seattle Hotel in Seattle, Washington.",
+        "place": "Seattle, Washington"
+    }, 
+    { 
+        "indexId": "test011",
+        "title": "Snowstorm Concentrator, Troy, Montana. No. 6.",
+        "creator": null,
+        "subjects": ["mining","mines (extracting complexes)","mine buildings","men","photographic postcards"],
+        "date": "1911",
+        "description": "Postcard of the concentrator building of the Snow Storm mine in Troy, Montana.",
+        "place": "Troy, Montana"
+    }, 
+    { 
+        "indexId": "test012",
+        "title": "Natatorium, Boise, Idaho",
+        "creator": "Wesley Andrews, Inc.",
+        "subjects": ["swimming pools","trees","buildings","indoor swimming pools","photographic postcards"],
+        "date": "1912",
+        "description": "Postcard is a photograph of a geothermal swimming pool that was located in Boise, Idaho.",
+        "place": "Boise, Idaho"
+    }, 
+    { 
+        "indexId": "test013",
+        "title": "C. Weisgerber Brewery. Lewiston, Idaho.",
+        "creator": null,
+        "subjects": ["breweries"],
+        "date": "1905",
+        "description": "Image of brewery building with horse cart and dog sitting on the street.",
+        "place": "Lewiston, Idaho"
+    }, 
+    { 
+        "indexId": "test014",
+        "title": "Albion State Normal School. Old Administration Building. Albion, Idaho.",
+        "creator": "Engstrom Studio, Lewiston",
+        "subjects": ["schools"],
+        "date": "1915",
+        "description": "Photograph of old Administration Building.",
+        "place": "Albion, Idaho"
+    }, 
+    { 
+        "indexId": "test015",
+        "title": "Cottonwood Public School [under construction?]. Cottonwood, Idaho.",
+        "creator": null,
+        "subjects": ["schools"],
+        "date": "1912",
+        "description": "Twelve men posed outside building",
+        "place": "Cottonwood, Idaho"
+    }];
